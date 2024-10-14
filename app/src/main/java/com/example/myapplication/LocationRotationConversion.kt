82801358775fbe6607577276da5ea1468e88f53a
Liam <brownliamm@gmail.com>
2024-10-14 13:07:41 -0400

    outline ConvertLocation

--- app/src/main/java/com/example/myapplication/LocationRotationConversion.kt
+++ app/src/main/java/com/example/myapplication/LocationRotationConversion.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
+import kotlin.math.*
 
-val LOCPOS = arrayOf(
+private val LOCPOS = arrayOf(
     Pair(42.730495, -73.678432),
     Pair(42.73236423119569, -73.67009163262247),
     Pair(42.72706080812248, -73.67660537697532),
@@ -10,7 +11,7 @@ val LOCPOS = arrayOf(
     Pair(42.73078307416792, -73.67722000350271)
 )
 
-val LOCMAP = arrayOf(
+private val LOCMAP = arrayOf(
     Pair(587, 749),
     Pair(1121, 584),
     Pair(707, 1047),
@@ -19,3 +20,24 @@ val LOCMAP = arrayOf(
     Pair(274, 925),
     Pair(668, 724)
 )
+
+private val TESTSIZE = LOCPOS.size
+private val COMBOS: Int = TESTSIZE * (TESTSIZE - 1) / 2
+private var scale_factor = 0.0
+private var radian_displace = 0.0
+
+//private fun get_scale_factor(): Double
+
+//private fun get_radian_displace(): Double
+
+//private fun dot_product(a: Pair<Double, Double>, b: Pair<Double, Double>): Double
+
+//private fun magnitute(mypoint: Pair<Double, Double>): Double
+
+//private fun distance_differences(mypoints: Pair<Double, Double>): Array<Pair<Double, Double>>
+
+//private fun mean_point(mypoints: Pair<Double, Double>): Pair<Double, Double>
+
+//private fun rotate_point(mypoint: Pair<Double, Double>, angle: Double): Pair<Double, Double>
+
+//fun ConvertLocation(latitude: Double, longitude: Double): Pair<Double, Double>
