daf928d93d349e89140ff9042eae69884c20024e
Liam <brownliamm@gmail.com>
2024-10-21 22:51:27 -0400

    add SearchableNode to Graph.kt

--- app/src/main/java/com/example/myapplication/Graph.kt
+++ app/src/main/java/com/example/myapplication/Graph.kt
@@ -3,3 +3,7 @@ package com.example.myapplication
 open class Node(val position: Pair<Double, Double>) {
 
 }
+
+open class SearchableNode(position: Pair<Double, Double>, val image_position: Pair<Int, Int>, val name: String) : Node(position) {
+
+}
