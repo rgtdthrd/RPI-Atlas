e7eea62bb9632b7dcade2a441c3cccf30d543e4f
Liam <brownliamm@gmail.com>
2024-10-21 22:52:12 -0400

    add Graph to Graph.kt

--- app/src/main/java/com/example/myapplication/Graph.kt
+++ app/src/main/java/com/example/myapplication/Graph.kt
@@ -1,5 +1,23 @@
 package com.example.myapplication
 
+class Graph() {
+    var nodes = emptyArray<Node>()
+
+    fun AddNode(new_node: Node) {
+        nodes += new_node
+    }
+
+    fun GetAllSearchableNodeNames(): Array<String> {
+        var out_array = emptyArray<String>()
+        for (node in nodes) {
+            if (node is SearchableNode) {
+                out_array += node.name
+            }
+        }
+        return out_array
+    }
+}
+
 open class Node(val position: Pair<Double, Double>) {
 
 }
