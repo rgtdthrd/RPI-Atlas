de822ef22832dc90d1df6f06fdbfb6e9fe14fc22
Liam <brownliamm@gmail.com>
2024-10-27 17:12:39 -0400

    add latitude and longitude to location_info_and_options.xml

--- app/src/main/java/com/example/myapplication/LocationInfoOptions.kt
+++ app/src/main/java/com/example/myapplication/LocationInfoOptions.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -10,10 +11,13 @@ import android.widget.ImageButton
 private var seed_node = SearchableNode(Pair(0.0, 0.0), "N/A")
 
 class LocationInfoAndOptionsActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.location_info_and_options)
         findViewById<TextView>(R.id.locationName).text = seed_node.name
+        findViewById<TextView>(R.id.latitude_text).text = "Latitude ${seed_node.position.first}"
+        findViewById<TextView>(R.id.longitude_text).text = "Longitude ${seed_node.position.second}"
 
         val backButton = findViewById<ImageButton>(R.id.backButton)
         backButton.setOnClickListener {
