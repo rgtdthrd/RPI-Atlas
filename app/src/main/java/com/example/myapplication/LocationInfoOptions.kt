e02550df2fcd1f0cf4dd07ba084615b5c821d782
Liam <brownliamm@gmail.com>
2024-10-27 16:27:27 -0400

    add location name to location_info_and_options.xml

--- app/src/main/java/com/example/myapplication/LocationInfoOptions.kt
+++ app/src/main/java/com/example/myapplication/LocationInfoOptions.kt
@@ -3,18 +3,23 @@ package com.example.myapplication
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
+
+private var seed_node = SearchableNode(Pair(0.0, 0.0), "N/A")
 
 class LocationInfoAndOptionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.location_info_and_options)
+        findViewById<TextView>(R.id.locationName).text = seed_node.name
     }
 }
 
 fun DisplayLocationInfo(context: Context, locationNode: SearchableNode) {
-    println("Location: ${locationNode.name}")
+    seed_node = locationNode
     val intent = Intent(context, LocationInfoAndOptionsActivity::class.java)
     context.startActivity(intent)
+
 }
